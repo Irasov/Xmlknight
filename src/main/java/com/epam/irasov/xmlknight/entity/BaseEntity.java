b03dfef81ead6bc866cd9a006b5a6f61dc93d192
Irasov <srnan@yandex.ru>
2015-07-01 17:32:57 +0600

    BaseEntity

---
+++ src/main/java/com/epam/irasov/xmlknight/entity/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.epam.irasov.xmlknight.entity;
+
+import java.util.UUID;
+
+public abstract class BaseEntity {
+    private Long id;
+    private UUID uuid;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return !(uuid != null ? !uuid.equals(that.uuid) : that.uuid != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (uuid != null ? uuid.hashCode() : 0);
+        return result;
+    }
+}
