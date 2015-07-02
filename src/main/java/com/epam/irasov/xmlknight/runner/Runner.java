cdefa71485129b172f3580b2be763ebe7d8cb4a9
Irasov <srnan@yandex.ru>
2015-07-02 19:54:52 +0600

    not stax

--- src/main/java/com/epam/irasov/xmlknight/runner/Runner.java
+++ src/main/java/com/epam/irasov/xmlknight/runner/Runner.java
@@ -8,6 +8,7 @@ import com.epam.irasov.xmlknight.logic.Action;
 import com.epam.irasov.xmlknight.parser.Parsers;
 import com.epam.irasov.xmlknight.parser.ParserFactoryKnight;
 import com.epam.irasov.xmlknight.reporter.Report;
+import com.epam.irasov.xmlknight.util.PropertyManager;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,7 +19,7 @@ public class Runner {
     public static final int CUSTOM_MIN_PRICE = 500;
     public static final int CUSTOM_MAX_PRICE = 1500;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         ParserFactoryKnight knightParserFactory = new ParserFactoryKnight();
         Parsers parser = knightParserFactory.createKnightParser("sax");
         InputStream inputStream = Runner.class.getClassLoader().getResourceAsStream("knight.xml");
@@ -26,9 +27,8 @@ public class Runner {
         Action.sortAmmunition(knight.getAmmunitionList(), CUSTOM_SORT_CRITERION);
         Report.report(knight);
         Report.report(Action.searchPrice(knight.getAmmunitionList(), CUSTOM_MIN_PRICE, CUSTOM_MAX_PRICE));
-
-       /* Parsers parser2 = knightParserFactory.createKnightParser("stax");
+     /*   Parsers parser2 = knightParserFactory.createKnightParser("stax");
         Knight knight1 = parser2.knightParser(inputStream);
-        System.out.println(knight1.toString());*/
+        System.out.println(knight1);*/
     }
 }
