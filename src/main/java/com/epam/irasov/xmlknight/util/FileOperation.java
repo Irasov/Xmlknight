9379fe4cd628af32900d071c24ed780b8248726f
Irasov <srnan@yandex.ru>
2015-07-05 16:33:53 +0600

    LOGGER

---
+++ src/main/java/com/epam/irasov/xmlknight/util/FileOperation.java
@@ -0,0 +1,19 @@
+package com.epam.irasov.xmlknight.util;
+
+import org.apache.log4j.Logger;
+
+import java.io.InputStream;
+
+public class FileOperation {
+    private static Logger LOGGER = Logger.getLogger(FileOperation.class);
+
+    public static InputStream LoadProperty(String filename) {
+        try {
+            InputStream in = FileOperation.class.getClassLoader().getResourceAsStream(filename);
+            return in;
+        } catch (IllegalArgumentException e) {
+            LOGGER.info("FILE NOT FOUND" + e);
+        }
+        return null;
+    }
+}
