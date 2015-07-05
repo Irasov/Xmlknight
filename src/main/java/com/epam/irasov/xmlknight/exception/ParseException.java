79466ff064f95cc506aa27a7e1fd78ddd3919f13
Irasov <srnan@yandex.ru>
2015-07-05 16:28:03 +0600

    LOGGER

--- src/main/java/com/epam/irasov/xmlknight/exception/ParseException.java
+++ src/main/java/com/epam/irasov/xmlknight/exception/ParseException.java
@@ -1,16 +1,20 @@
 package com.epam.irasov.xmlknight.exception;
 
+import org.apache.log4j.Logger;
+
 public class ParseException extends RuntimeException {
+    private static Logger LOGGER = Logger.getLogger(ParseException.class);
     public ParseException(Throwable cause){
-        super(cause);
+        LOGGER.info(cause);
     }
 
     public ParseException(String message,Throwable cause){
-        super(message,cause);
+        LOGGER.info(message);
+        LOGGER.info(cause);
     }
 
     public ParseException(String message){
-        super(message);
+        LOGGER.info(message);
     }
     public ParseException(){
 
