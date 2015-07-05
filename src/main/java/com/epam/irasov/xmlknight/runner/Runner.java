e66ed4b2e94b7b0944ed98f447c2169b6b7666af
Irasov <srnan@yandex.ru>
2015-07-05 16:33:05 +0600

    LOGGER

--- src/main/java/com/epam/irasov/xmlknight/runner/Runner.java
+++ src/main/java/com/epam/irasov/xmlknight/runner/Runner.java
@@ -1,30 +1,38 @@
 package com.epam.irasov.xmlknight.runner;
 
-import com.epam.irasov.xmlknight.entity.Knight;
+import com.epam.irasov.xmlknight.entity.*;
 import com.epam.irasov.xmlknight.logic.Action;
-import com.epam.irasov.xmlknight.parser.Parsers;
-import com.epam.irasov.xmlknight.parser.ParserFactoryKnight;
+import com.epam.irasov.xmlknight.parser.Parser;
+import com.epam.irasov.xmlknight.parser.KnightParserFactory;
 import com.epam.irasov.xmlknight.reporter.Report;
+import com.epam.irasov.xmlknight.util.PropertyManagerBodyTag;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class Runner {
     public static final String CUSTOM_SORT_CRITERION = "weight";
     public static final int CUSTOM_MIN_PRICE = 500;
     public static final int CUSTOM_MAX_PRICE = 1500;
+    public static final String XML_FILE = "knight.xml";
+    public static final String CUSTOM_PARSER = "sax";
+    private static Logger LOGGER = Logger.getLogger(Runner.class);
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        ParserFactoryKnight knightParserFactory = new ParserFactoryKnight();
-        Parsers parser = knightParserFactory.createKnightParser("sax");
-        InputStream inputStream = Runner.class.getClassLoader().getResourceAsStream("knight.xml");
-        Knight knight = parser.knightParser(inputStream);
-        Action.sortAmmunition(knight.getAmmunitionList(), CUSTOM_SORT_CRITERION);
-        Report.report(knight);
-        Report.report(Action.searchPrice(knight.getAmmunitionList(), CUSTOM_MIN_PRICE, CUSTOM_MAX_PRICE));
-        String s = "Armor.ley";
-     /*   Parsers parser2 = knightParserFactory.createKnightParser("stax");
-        Knight knight1 = parser2.knightParser(inputStream);
-        System.out.println(knight1);*/
+        KnightParserFactory knightParserFactory = new KnightParserFactory();
+        Parser parser = knightParserFactory.createKnightParser(CUSTOM_PARSER);
+        try {
+            InputStream inputStream = Runner.class.getClassLoader().getResourceAsStream(XML_FILE);
+            Knight knight = parser.knightParser(inputStream);
+            Action.sortAmmunition(knight.getAmmunitionList(), CUSTOM_SORT_CRITERION);
+            Report.report(knight);
+            Report.report(Action.searchPrice(knight.getAmmunitionList(), CUSTOM_MIN_PRICE, CUSTOM_MAX_PRICE));
+        }catch (IllegalArgumentException e){
+            LOGGER.info("FILE NOT FOUND: "+e);
+        }
     }
 }
