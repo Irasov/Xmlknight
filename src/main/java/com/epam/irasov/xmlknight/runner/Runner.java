055113cbe59ed5aa22070601b32d5933f491503a
Irasov <srnan@yandex.ru>
2015-07-07 17:33:23 +0600

    STAX

--- src/main/java/com/epam/irasov/xmlknight/runner/Runner.java
+++ src/main/java/com/epam/irasov/xmlknight/runner/Runner.java
@@ -5,14 +5,10 @@ import com.epam.irasov.xmlknight.logic.Action;
 import com.epam.irasov.xmlknight.parser.Parser;
 import com.epam.irasov.xmlknight.parser.KnightParserFactory;
 import com.epam.irasov.xmlknight.reporter.Report;
-import com.epam.irasov.xmlknight.util.PropertyManagerBodyTag;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 public class Runner {
     public static final String CUSTOM_SORT_CRITERION = "weight";
@@ -25,7 +21,7 @@ public class Runner {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         KnightParserFactory knightParserFactory = new KnightParserFactory();
-        //Parser parser = knightParserFactory.createKnightParser(CUSTOM_SAX_PARSER);
+       // Parser parser = knightParserFactory.createKnightParser(CUSTOM_SAX_PARSER);
        Parser parser = knightParserFactory.createKnightParser(CUSTOM_STAX_PARSER);
         try {
             InputStream inputStream = Runner.class.getClassLoader().getResourceAsStream(XML_FILE);
