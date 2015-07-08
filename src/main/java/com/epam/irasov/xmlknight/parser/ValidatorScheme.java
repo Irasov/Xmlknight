b3791b3d7d31cebc6cb43831919d050d71f84fc3
Irasov <srnan@yandex.ru>
2015-07-08 18:27:04 +0600

    ERROR_HANDLER

--- src/main/java/com/epam/irasov/xmlknight/parser/ValidatorScheme.java
+++ src/main/java/com/epam/irasov/xmlknight/parser/ValidatorScheme.java
@@ -1,7 +1,10 @@
 package com.epam.irasov.xmlknight.parser;
 
 import com.epam.irasov.xmlknight.exception.ParseException;
+import org.apache.log4j.Logger;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.XMLConstants;
@@ -15,6 +18,7 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class ValidatorScheme {
+    private static Logger LOGGER = Logger.getLogger(ValidatorScheme.class);
 
     public static void validator(){
 
@@ -33,17 +37,32 @@ public class ValidatorScheme {
         factory.setNamespaceAware(true);
         factory.setSchema(schema);
 
-        SAXParser parser;
+        SAXParser parser = null;
         try {
             parser = factory.newSAXParser();
             parser.parse(xmlIn, new MyHandler());
         } catch (ParserConfigurationException | SAXException|IOException e) {
             throw new ParseException(e);
         }
+        LOGGER.info("successfully tested scheme");
 
     }
 
-    private  static class MyHandler extends DefaultHandler{
+    private  static class MyHandler extends DefaultHandler implements ErrorHandler {
 
+        @Override
+        public void warning(SAXParseException exception) throws SAXException {
+            LOGGER.info("Warning: "+exception);
+        }
+
+        @Override
+        public void error(SAXParseException exception) throws SAXException {
+            LOGGER.info("ERROR: "+exception);
+        }
+
+        @Override
+        public void fatalError(SAXParseException exception) throws SAXException {
+            LOGGER.info("FatalError: "+exception);
+        }
     }
 }
